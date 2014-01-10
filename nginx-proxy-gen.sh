6b035f5e147a01f9ae07e3c4c2d6595ebb92e59d
Ubuntu <ubuntu@ip-172-31-7-80.ap-southeast-2.compute.internal>
2014-01-10 04:06:52 +0000

    Finished basic proxy gen script

--- nginx-proxy-gen.sh
+++ nginx-proxy-gen.sh
@@ -1,1 +1,17 @@
+
+# Make sure to run apt-get update first. 
+echo 'Installing nginx...' 
 sudo apt-get install -y nginx
+
+# Overwrite nginx.conf with proxy.conf file. TODO Perhaps it would
+# be better to just create a new conf in sites-available?
+echo 'Overwriting nginx.conf...'   
+sudo cp ./proxy.conf /etc/nginx/nginx.conf 
+
+echo 'Starting nginx...'
+sudo nginx
+
+echo 'Ensuring nginx will run on startup...' 
+sudo update-rc.d nginx defaults 
+
+echo 'Proxy started on port 80! Stop the proxy by running "sudo nginx -s stop"'
