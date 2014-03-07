75dcac42b5706a9be664fd23c0d8afcaf4975eb4
root <root@ebla-connect.(none)>
2014-03-07 05:36:10 +0000

    Updated configuration and added generation of proxy auth credentials

--- nginx-proxy-gen.sh
+++ nginx-proxy-gen.sh
@@ -2,16 +2,21 @@
 # Make sure to run apt-get update first. 
 echo 'Installing nginx...' 
 sudo apt-get install -y nginx
+echo 'Installing apache2-utils' 
+apt-get install -y apache2-utils
 
 # Overwrite nginx.conf with proxy.conf file. TODO Perhaps it would
 # be better to just create a new conf in sites-available?
 echo 'Overwriting nginx.conf...'   
 sudo cp ./proxy.conf /etc/nginx/nginx.conf 
 
-echo 'Starting nginx...'
-sudo nginx
-
 echo 'Ensuring nginx will run on startup...' 
 sudo update-rc.d nginx defaults 
+
+echo 'Creating proxy auth credentials...' 
+htpasswd -c /etc/nginx/conf.d/.htpasswd proxy-clienti
+
+echo 'Starting nginx...'
+sudo nginx
 
 echo 'Proxy started on port 80! Stop the proxy by running "sudo nginx -s stop"'
